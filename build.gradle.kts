63fc0aa8d455794da1ce16ae109f8612a0dad8b5
sawafrolov <frolov@devs-vault.com>
2023-12-04 20:19:30 +0400

    Основа Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "com.github.sawafrolov"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
