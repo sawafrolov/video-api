64a6a8ec2a62ad4c9cb8eadc6866769b99a526b0
sawafrolov <frolov@devs-vault.com>
2023-12-05 06:54:11 +0400

    Main class

---
+++ src/main/java/com/github/sawafrolov/videoapi/VideoApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.sawafrolov.videoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoApiApplication.class, args);
+    }
+}
