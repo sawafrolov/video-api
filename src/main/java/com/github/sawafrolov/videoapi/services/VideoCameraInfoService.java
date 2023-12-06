af58f7e1d40bd16d2f46775055ddfd4cd625aef5
sawafrolov <frolov@devs-vault.com>
2023-12-06 08:43:52 +0400

    Удалил лишнее

--- src/main/java/com/github/sawafrolov/videoapi/services/VideoCameraInfoService.java
+++ src/main/java/com/github/sawafrolov/videoapi/services/VideoCameraInfoService.java
@@ -3,14 +3,7 @@ package com.github.sawafrolov.videoapi.services;
 import com.github.sawafrolov.videoapi.dto.VideoCameraInfoDto;
 import reactor.core.publisher.Flux;
 
-/**
- * Сервис для получения данных о видеокамерах от API
- */
 public interface VideoCameraInfoService {
 
-    /**
-     * Получает данные о видеокамерах от API
-     * @return Данные о видеокамерах
-     */
     Flux<VideoCameraInfoDto> collectVideoCameraInfo();
 }
