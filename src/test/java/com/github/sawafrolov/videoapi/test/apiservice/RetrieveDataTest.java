6790f5a153330063aed5ac518c6686eaa7d88d2f
sawafrolov <frolov@devs-vault.com>
2023-12-09 06:53:19 +0400

    Тестирование ApiService

--- src/test/java/com/github/sawafrolov/videoapi/test/apiservice/RetrieveDataTest.java
+++ src/test/java/com/github/sawafrolov/videoapi/test/apiservice/RetrieveDataTest.java
@@ -8,18 +8,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.github.sawafrolov.videoapi.util.DtoUtil.apiData;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class RetrieveDataTest {
 
     @Value("${video-api.api-service-test.incorrect-urls.video-api-url}")
     private String incorrectApiUrl;
 
+    @Value("${video-api.api-service-test.video-api-url}")
+    private String apiUrl;
+
     @Autowired
     private ApiService apiService;
 
     @Test
     @DisplayName("Check retrieve json array from API with null API URL")
-    public void testRetrieveFluxUrlNull() {
+    public void testRetrieveDataUrlNull() {
         apiService
                 .retrieveData(null, VideoCameraDto.class)
                 .map(Object::hashCode)
@@ -28,10 +34,19 @@ public class RetrieveDataTest {
 
     @Test
     @DisplayName("Check retrieve json array from API with incorrect API URL")
-    public void testRetrieveFluxUrlIncorrect() {
+    public void testRetrieveDataUrlIncorrect() {
         apiService
                 .retrieveData(incorrectApiUrl, VideoCameraDto.class)
                 .map(Object::hashCode)
                 .subscribe(System.out::println);
+    }
+
+    @Test
+    @DisplayName("Check retrieve json array from API")
+    public void testRetrieveData() {
+        apiService
+                .retrieveData(apiUrl, VideoCameraDto.class)
+                .collectList()
+                .subscribe(apiData -> assertEquals(apiData, apiData()));
     }
 }
--- src/test/java/com/github/sawafrolov/videoapi/util/DtoUtil.java
+++ src/test/java/com/github/sawafrolov/videoapi/util/DtoUtil.java
@@ -4,12 +4,46 @@ import com.github.sawafrolov.videoapi.dto.TokenDto;
 import com.github.sawafrolov.videoapi.dto.VideoCameraDto;
 import com.github.sawafrolov.videoapi.dto.VideoStreamDto;
 import com.github.sawafrolov.videoapi.dto.VideoStreamUrlType;
-import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 public class DtoUtil {
 
-    public static Flux<VideoCameraDto> apiResult() {
-        return null;
+    public static List<VideoCameraDto> apiData() {
+        VideoCameraDto videoCameraDto1 = VideoCameraDto
+                .builder()
+                .id(1L)
+                .sourceDataUrl("https://run.mocky.io/v3/e417b132-12ff-434d-a2c2-f81e6ddc1c6c")
+                .tokenDataUrl("https://run.mocky.io/v3/fc0b6f75-b911-4b0a-9a1d-dc2379a6ef1c")
+                .build();
+
+        VideoCameraDto videoCameraDto2 = VideoCameraDto
+                .builder()
+                .id(2L)
+                .sourceDataUrl("https://run.mocky.io/v3/86da6879-1444-4c92-818c-77b55d9c7864")
+                .tokenDataUrl("https://run.mocky.io/v3/8b3babb6-c354-4874-a878-68d883c8425d")
+                .build();
+
+        VideoCameraDto videoCameraDto3 = VideoCameraDto
+                .builder()
+                .id(3L)
+                .sourceDataUrl("https://run.mocky.io/v3/64ab5cc8-1f47-4ac5-a621-90d4410f7db7")
+                .tokenDataUrl("https://run.mocky.io/v3/1ea436bd-1d3f-423c-9373-19e7d825dcd7")
+                .build();
+
+        VideoCameraDto videoCameraDto20 = VideoCameraDto
+                .builder()
+                .id(20L)
+                .sourceDataUrl("https://run.mocky.io/v3/e8eee697-3018-4b2b-8b05-d00ab6b545c2")
+                .tokenDataUrl("https://run.mocky.io/v3/e2d29bba-4315-453b-b270-e668b2a67af1")
+                .build();
+
+        return List.of(
+                videoCameraDto1,
+                videoCameraDto2,
+                videoCameraDto3,
+                videoCameraDto20
+        );
     }
 
     public static VideoStreamDto videoStreamDtoId2() {
